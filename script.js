6a83590052afa428677fd1814ef318741ef0976f
anhar <rimbindo@gmail.com>
2026-02-14 18:09:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -587,10 +587,10 @@ scene.add(planet);
 
 //Ganti Deskripsi Planet
 const ringTexts = [
-  'Galaxy of love From ...',//untuk deskripsi planet layer 1
-  "I love you",//untuk deskripsi planet layer 2
-  "♡Happy Girlfriend day♡",//untuk deskripsi planet layer 3
-  "01/08/2025",//untuk deskripsi planet layer 4
+  'Galaxy of love From Anhar',//untuk deskripsi planet layer 1
+  "I will never stop loving u",//untuk deskripsi planet layer 2
+  "♡Happy valentines day♡",//untuk deskripsi planet layer 3
+  "14/02/2026",//untuk deskripsi planet layer 4
   ...(window.dataCCD && window.dataCCD.data.ringTexts ? window.dataCCD.data.ringTexts : [])
 ];
 
